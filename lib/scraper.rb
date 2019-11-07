31ff1ae84673ee55e2942fb19b89c9f5feed6830
Ally Morson <ally.morson@gmail.com>
2019-11-07 15:44:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
  
 class Scraper
  
-  def get_page
+  def get_page #responsible for just getting the page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
  
