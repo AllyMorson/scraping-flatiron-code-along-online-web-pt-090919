0272deddecd1b9b9602c15703e5341e5d1381e87
Ally Morson <ally.morson@gmail.com>
2019-11-07 15:47:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
  
-  def get_courses #operate on the HTML page
+  def get_courses #operate on the HTML page and return the collection of Nokogiri XML elements that describe each course.
     self.get_page.css(".post")
   end
  
